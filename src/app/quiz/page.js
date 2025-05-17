04d6892d5c366e7fe3489854c05328d620d7ec3b
seming2 <popqr3@naver.com>
2025-05-17 18:09:00 +0900

    quiz section title 수정

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -5,7 +5,7 @@ import quizData from '@/data/quiz/quizData';
 export default function QuizPage() {
   return (
     <main className='max-w-5xl mx-auto py-12 px-4'>
-      <h1 className='text-3xl font-bold mb-8 text-center text-gray-900'>React vs Next.js 퀴즈</h1>
+      <h1 className='text-3xl font-bold mb-8 text-center text-gray-900'>Quiz ✅</h1>
       <QuizContent quizData={quizData} />
     </main>
   );
